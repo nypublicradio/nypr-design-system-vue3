a46bfcd9caac21aa0766f5271acfc6975e1f2139
Thomas Bono <thomasbono.com@gmail.com>
2022-04-29 16:02:16 -0400

    vitest set up and working 100%

---
+++ v2/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const times = ref(2)
+const props = defineProps({ count: 0 })
+
+const result = computed(() => props.count * times.value)
+
+defineExpose(props)
+</script>
+
+<template>
+  <div>{{ count }} x {{ times }} = {{ result }}</div>
+  <button @click="times += 1">
+    x1
+  </button>
+</template>
