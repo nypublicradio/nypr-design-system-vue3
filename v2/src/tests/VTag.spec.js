b13edd79b857d3bc3b59ce5f62b6ebf8a16d9d8c
Thomas Bono <thomasbono.com@gmail.com>
2022-05-02 12:33:51 -0400

    media embed updated

---
+++ v2/src/tests/VTag.spec.js
@@ -0,0 +1,51 @@
+import { mount } from '@vue/test-utils'
+import VTag from '../components/VTag.vue'
+import { toHaveNoViolations } from 'jest-axe'
+import axe from './axe-helper'
+
+expect.extend(toHaveNoViolations)
+
+describe('VTag', () => {
+  const name = 'news'
+  const slug = 'news'
+  // const radius = 10
+
+  test('text prop works: null', () => {
+    const wrapper = mount(VTag, {
+      props: {
+        name,
+        slug,
+        stubs: ['nuxt-link']
+      }
+    })
+    // check if prop works
+    const button = wrapper.find('.flexible-link .p-button .p-button-label')
+    expect(button.text()).toBe(name)
+  })
+
+  // this is using nuxt-link, so we need to find the "to" attribute instead of "href"
+  test('link attribute works', () => {
+    const wrapper = mount(VTag, {
+      props: {
+        name,
+        slug,
+        stubs: ['nuxt-link']
+      }
+    })
+    // check if prop works and rendered correctly
+    const div = wrapper.find('.flexible-link')
+    expect(div.attributes().to).toBe(slug)
+  })
+
+  test('it passes basic accessibility tests', async () => {
+    const wrapper = mount(VTag, {
+      props: {
+        name,
+        slug,
+        stubs: ['nuxt-link']
+      }
+    })
+    const results = await axe(wrapper.element)
+    expect(results).toHaveNoViolations()
+  })
+}) 
