b291527b7385028b768ad14a3770c143a49c2013
Thomas Bono <thomasbono.com@gmail.com>
2022-07-08 10:21:44 -0400

    deepsource updates, toast fix

--- plugins/primevue.js
+++ plugins/primevue.js
@@ -7,14 +7,15 @@ import Image from "primevue/image/image.esm.js"
 
 
 import PrimeVue from 'primevue/config/config.esm.js'
-import AutoComplete from 'primevue/autocomplete/autocomplete.esm.js'
 import Accordion from 'primevue/accordion/accordion.esm.js'
 import AccordionTab from 'primevue/accordiontab/accordiontab.esm.js'
+import AutoComplete from 'primevue/autocomplete/autocomplete.esm.js'
 import Button from 'primevue/button/button.esm.js'
 import Calendar from 'primevue/calendar/calendar.esm.js'
 import Checkbox from 'primevue/checkbox/checkbox.esm.js'
 import Chips from 'primevue/chips/chips.esm.js'
 import Column from 'primevue/column/column.esm.js'
+import ConfirmationService from 'primevue/confirmationservice/confirmationservice.esm.js'
 import ConfirmDialog from 'primevue/confirmdialog/confirmdialog.esm.js'
 import ConfirmPopup from 'primevue/confirmpopup/confirmpopup.esm.js'
 import ContextMenu from 'primevue/contextmenu/contextmenu.esm.js'
@@ -22,6 +23,7 @@ import DataTable from 'primevue/datatable/datatable.esm.js'
 import Dialog from 'primevue/dialog/dialog.esm.js'
 import Dropdown from 'primevue/dropdown/dropdown.esm.js'
 import Fieldset from 'primevue/fieldset/fieldset.esm.js'
+import InlineMessage from 'primevue/inlinemessage/inlinemessage.esm.js'
 import InputNumber from 'primevue/inputnumber/inputnumber.esm.js'
 import InputSwitch from 'primevue/inputswitch/inputswitch.esm.js'
 import InputText from 'primevue/inputtext/inputtext.esm.js'
@@ -33,26 +35,25 @@ import MultiSelect from 'primevue/multiselect/multiselect.esm.js'
 import OverlayPanel from 'primevue/overlaypanel/overlaypanel.esm.js'
 import Panel from 'primevue/panel/panel.esm.js'
 import ProgressBar from 'primevue/progressbar/progressbar.esm.js'
-import Rating from 'primevue/rating/rating.esm.js'
 import RadioButton from 'primevue/radiobutton/radiobutton.esm.js'
+import Rating from 'primevue/rating/rating.esm.js'
 import Ripple from 'primevue/ripple/ripple.esm.js'
 import SelectButton from 'primevue/selectbutton/selectbutton.esm.js'
 import Slider from 'primevue/slider/slider.esm.js'
 import SplitButton from 'primevue/splitbutton/splitbutton.esm.js'
-import TieredMenu from 'primevue/tieredmenu/tieredmenu.esm.js'
-import Textarea from 'primevue/textarea/textarea.esm.js'
-import Toast from 'primevue/toast/toast.esm.js'
-import ToastService from 'primevue/toastservice/toastservice.esm.js'
-import ConfirmationService from 'primevue/confirmationservice/confirmationservice.esm.js'
 import TabView from 'primevue/tabview/tabview.esm.js'
 import TabPanel from 'primevue/tabpanel/tabpanel.esm.js'
+import Textarea from 'primevue/textarea/textarea.esm.js'
+import TieredMenu from 'primevue/tieredmenu/tieredmenu.esm.js'
+import Toast from 'primevue/toast/toast.esm.js'
+import ToastService from 'primevue/toastservice/toastservice.esm.js'
 import Timeline from 'primevue/timeline/timeline.esm.js'
 import ToggleButton from 'primevue/togglebutton/togglebutton.esm.js'
 import Tooltip from 'primevue/tooltip/tooltip.esm.js'
-import InlineMessage from 'primevue/inlinemessage/inlinemessage.esm.js'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+    nuxtApp.vueApp.use(ToastService)
 
     nuxtApp.vueApp.component('Image', Image)
 
@@ -92,6 +93,8 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('Textarea', Textarea)
     nuxtApp.vueApp.component('TieredMenu', TieredMenu)
     nuxtApp.vueApp.component('Toast', Toast)
+    nuxtApp.vueApp.component('ToastService', ToastService)
+    nuxtApp.vueApp.component('ConfirmationService', ConfirmationService)
     nuxtApp.vueApp.component('ToggleButton', ToggleButton)
     nuxtApp.vueApp.component('Timeline', Timeline)
     nuxtApp.vueApp.component('InlineMessage', InlineMessage)
