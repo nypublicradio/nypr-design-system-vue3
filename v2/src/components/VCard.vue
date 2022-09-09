85aa5ca42fc44bec1b042367d4f7a4453fc60ef1
Thomas Bono <thomasbono.com@gmail.com>
2022-09-09 17:35:57 -0400

    image quality and sizes defaults update

--- v2/src/components/VCard.vue
+++ v2/src/components/VCard.vue
@@ -116,7 +116,7 @@ const props = defineProps({
   /** * jpg compression quality */
   quality: {
     type: Number,
-    default: 85,
+    default: 70,
   },
   /** * bool to NOT use the variable quality calc based on sizes */
   flatQuality: {
@@ -126,7 +126,7 @@ const props = defineProps({
   sizes: {
     type: Array,
     default() {
-      return [2, 3, 4]
+      return [2, 3]
     },
   },
 })
