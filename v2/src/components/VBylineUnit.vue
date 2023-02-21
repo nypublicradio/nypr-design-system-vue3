4949357fb30cdeeddb58fc07c4bb118e9190d00a
walsh9 <walsh9@users.noreply.github.com>
2023-02-21 16:48:57 -0500

    add events to v-byline (#131)

--- v2/src/components/VBylineUnit.vue
+++ v2/src/components/VBylineUnit.vue
@@ -6,11 +6,18 @@ const props = defineProps({
     default: null,
   },
 })
+const emit = defineEmits([
+  'name-click', 'organization-click'
+])
 </script>
 
 <template>
   <div>
-    <v-flexible-link :to="author.url || null" class="v-byline-author-name">
+    <v-flexible-link
+      :to="author.url || null"
+      class="v-byline-author-name"
+      @click="emit('name-click', {text: `${author.firstName} ${author.lastName}`, url: author.url})"
+    >
       {{ author.firstName }} {{ author.lastName }}
     </v-flexible-link>
     <span v-if="author.organization" class="v-byline-parens"> (</span>
@@ -18,6 +25,7 @@ const props = defineProps({
       v-if="author.organization"
       :to="author.organizationUrl || null"
       class="v-byline-contributing-org"
+      @click="emit('organization-click', {text: author.organization, url: author.organizationUrl})"
     >
       {{ author.organization }}
     </v-flexible-link>
