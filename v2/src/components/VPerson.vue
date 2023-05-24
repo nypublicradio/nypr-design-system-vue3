e05ce4aaf9e88466e4ba301dc023dc3f0754fa70
Thomas Bono <thomasbono.com@gmail.com>
2023-05-24 15:03:19 -0400

    replaceing vperson with goth author component

--- v2/src/components/VPerson.vue
+++ v2/src/components/VPerson.vue
@@ -1,500 +1,166 @@
 <script setup>
-import { gsap } from 'gsap'
-import { isInViewport } from '../mixins/helpers.js'
+import { ref, computed } from 'vue'
+import VFlexibleLink from './VFlexibleLink.vue'
+import VSimpleResponsiveImage from './VSimpleResponsiveImage.vue'
 import VShareTools from './VShareTools.vue'
 import VShareToolsItem from './VShareToolsItem.vue'
-import VImageWithCaption from './VImageWithCaption.vue'
-import Button from 'primevue/button'
-import { ref, shallowRef, computed, onBeforeMount, onMounted } from 'vue'
 
 const props = defineProps({
-  /**
-   *  A URL pointing to an image of the person.
-   */
-  image: {
+  profileId: {
     type: String,
     default: null,
   },
-  /**
-   *  image scale in its container in vertical orientation only, 1(8.333%) through 12(100%)
-   */
-  imgScale: {
-    type: String,
-    default: '5',
+  imageSize: {
+    type: Object,
+    default: { sm: 60, md: 86, lg: 157 },
   },
-  /**
-   *  adds a circle mask around the image.
-   */
-  circle: {
+  showCta: {
+    type: Boolean,
+    default: true,
+  },
+  sponsored: {
     type: Boolean,
     default: false,
   },
-  /**
-   *  youtube link to promo video
-   */
-  video: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  Full name of the person.
-   */
-  fullName: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  A URL pointing to the person's bio page or anywhere else they want to link to.
-   */
-  nameLink: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  Organization this person is from.
-   */
-  organization: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  link to the organization.
-   */
-  organizationLink: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  The person's role or job. e.g. Host, Guest, Author, etc.
-   */
-  role: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  Information about the author. e.g. "Jen is a blah blah..." etc.
-   */
-  blurb: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  maxium lines of text to show when truncarted
-   */
-  truncate: {
-    type: [Boolean, String],
-    default: false,
-  },
-  /**
-   *  persons website url
-   */
-  websiteUrl: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  persons website label
-   */
-  websiteLabel: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  persons email address
-   */
-  email: {
-    type: String,
-    default: null,
-  },
-  /**
-   *  persons phone number
-   */
-  phoneNumbers: {
-    type: Array,
-    default: null,
-  },
-  /**
-   *  social account array
-   */
-  social: {
-    type: Array,
-    default: null,
-  },
-  /**
-   *  horizontal(default), vertical, responsive (changes to vertical at the small break point)
-   */
-  orientation: {
-    type: String,
-    default: 'horizontal',
-  },
-  /**
-   *  orientation responsive break point
-   */
-  bp: {
-    type: String,
-    default: 'sm',
-  },
-  /**
-   *  If component is on the Author page, disables image and name links
-   */
-  onAuthorPage: {
+  staffPage: {
     type: Boolean,
     default: false,
   },
 })
 
-const readMore = shallowRef(false)
-const socialArrayData = ref([])
-const bpVar = ref(props.bp)
+const profile = ref(null)
 
-const thisPersonRef = ref('thisPersonRef')
-const blurbHolderRef = ref('blurbHolderRef')
-const blurbRef = ref('blurbRef')
-const imgRef = ref('imgRef')
+fetch('https://cms.demo.nypr.digital/api/v2/pages/19')
+  .then((response) => response.json())
+  .then((data) => (profile.value = data))
 
-const getSocialArray = computed(() => {
-  let socialArray = []
-  if (props.social) {
-    props.social.forEach((item) => {
-      socialArray.push({
-        name: item.name,
-        url: item.url,
-        icon: item.icon,
-      })
-    })
-  }
-  return socialArray
-})
+// const ctaText = ref(props.sponsored ? 'Learn More' : 'Read more')
+// const profileLink = ref(
+//   props.sponsored ? profile.value['link'] : profile.value['url']
+// )
 
-const cssVars = computed(() => {
-  return {
-    '--trunc-lines': props.truncate ? props.truncate : 'unset',
-  }
+const profileImageSizeLg = ref(157)
+const profileImageSizeMd = ref(86)
+const profileImageSizeSm = ref(60)
+const imageSize = computed(() => {
+  return props.staffPage ? profileImageSizeLg.value : profileImageSizeMd.value
 })
-const hasDetails = computed(() => {
-  return !!props.role || !!props.blurb || !!props.social || !!props.fullName
-})
-const organizationComputed = computed(() => {
-  return ' (' + props.organization + ')'
-})
-
-onBeforeMount(() => {})
-
-onMounted(() => {
-  socialArrayData.value = getSocialArray.value
-})
+const imageSizePx = ref(imageSize.value + 'px')
+// const accountNameFromUrl = (url) => {
+//   return url
+//     ?.split('/')
+//     .filter((str) => str !== '')
+//     .slice(-1)[0]
+// }
 </script>
 
 <template>
-  <div ref="thisPersonRef" class="person" :style="cssVars">
-    <div
-      class="person-inner grid grid-nogutter"
-      :class="[{ 'has-details': hasDetails }, { 'no-image': !props.image }]"
-    >
-      <!-- Image section -->
-      <div
-        v-if="image"
-        class="person-image-link"
-        :class="[
-          { [`col-12 ${bp}:col-4`]: orientation === 'responsive' },
-          { [`col-12 w-${imgScale} m-auto`]: orientation === 'vertical' },
-          { 'col-4': orientation === 'horizontal' },
-        ]"
-      >
-        <div
-          ref="imgRef"
-          class="visual-holder"
-          :class="[
-            {
-              [`w-${imgScale} m-auto ${bp}:w-12 responsive`]:
-                orientation === 'responsive',
-            },
-            { [`w-${imgScale} m-auto w-12`]: orientation === 'vertical' },
-          ]"
-        >
-          <div ref="visual" class="visual" :class="[circle ? 'circle' : '']">
-            <v-image-with-caption
-              class="person-image person-image-img"
-              :image="props.image"
-              :alt-text="props.fullName"
-              :image-url="props.nameLink ? props.nameLink : null"
-              :ratio="[1, 1]"
-              role="presentation"
-              loading="lazy"
-              decoding="async"
-              :width="400"
-              :height="400"
-            />
-          </div>
-        </div>
-      </div>
+  <div
+    v-if="profile"
+    class="author-profile grid grid-nogutter"
+    :class="[{ staffPage: props.staffPage }]"
+  >
+    <pre>{{ profile.photo.id }}</pre>
+    <pre>{{ imageSize }}</pre>
 
-      <!-- Detail section -->
-      <div
-        v-if="hasDetails"
-        ref="detailsRef"
-        class="person-details flex flex-column justify-content-center"
-        :class="[
-          {
-            [`col-12 p-2 text-center ${bp}:text-left ${bp}:col-8 ${bp}:p-3`]:
-              orientation === 'responsive',
-          },
-          { [`col-12 p-2 text-center`]: orientation === 'vertical' },
-          { 'col-8 p-3': orientation === 'horizontal' },
-        ]"
-      >
-        <div class="person-name-link-holder">
-          <a
-            v-if="props.fullName"
-            class="person-name-link"
-            role="link"
-            :class="!props.nameLink || onAuthorPage ? 'disabled' : ''"
-            :href="props.nameLink ? props.nameLink : null"
-          >
-            <div class="h2" v-html="props.fullName"></div>
-          </a>
-        </div>
-        <div v-if="role" class="person-role footer">
-          <span v-html="role" />
-          <a
-            v-if="organization"
-            :href="organizationLink"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="person-role footer organization"
-            :class="[organizationLink ? 'link' : 'no-link']"
-            v-html="organizationComputed"
-          />
-        </div>
-        <div ref="blurbHolderRef" class="blurbHolder">
-          <p
-            v-if="blurb"
-            ref="blurbRef"
-            :class="[truncate ? 'truncate' : '']"
-            class="blurb"
-            v-html="blurb"
-          />
-        </div>
-        <div>
-          <a
-            v-if="truncate"
-            ref="readMoreRef"
-            class="read-more"
-            @click="handleBlurb()"
-            >{{ readMore ? 'read less' : 'read more' }}</a
-          >
-        </div>
-        <v-share-tools
-          v-if="socialArrayData.length > 0"
-          class="social"
-          :class="[
-            `text-center justify-content-center ${bp}:justify-content-start ${bp}:text-left`,
-          ]"
+    <div class="col-fixed profile">
+      <div class="author-image">
+        <v-flexible-link
+          :to="profile.photo.url"
+          raw
+          :aria-hidden="true"
+          :tabindex="-1"
         >
-          <v-share-tools-item
-            v-for="(item, index) in socialArrayData"
-            :key="index"
-            :service="item.service"
-            :username="item.username"
-            :link="item.profileUrl"
-            :label="item.label"
-            :action="item.action"
-            :url="item.url"
+          <v-simple-responsive-image
+            :src="`https://cms.prod.nypr.digital/images/${profile.photo.id}/fill-%width%x%height%|format-webp|webpquality-%quality%`"
+            :width="imageSize"
+            :height="imageSize"
+            :sizes="[2]"
+            :ratio="[1, 1]"
+            :loading="props.staffPage ? 'eager' : 'lazy'"
+            alt=""
           />
-        </v-share-tools>
+          <!-- <img
+            v-else
+            src="/avatar.svg"
+            :loading="props.staffPage ? 'eager' : 'lazy'"
+            alt=""
+          /> -->
+        </v-flexible-link>
       </div>
     </div>
+    <!-- <div class="col flex flex-column gap-2">
+      <div
+        class="flex flex-column align-items-start gap-2 md:flex-row md:align-items-center"
+      >
+        <v-flexible-link :to="profileLink" class="no-underline">
+          <div class="h5">{{ profile.name }}</div>
+        </v-flexible-link>
+        <span v-if="profile.socialMediaProfile">
+          <v-share-tools class="">
+            <v-share-tools-item
+              v-for="account in profile.socialMediaProfile"
+              :key="account.id"
+              :service="account.service"
+              :username="accountNameFromUrl(account.profileUrl)"
+            />
+          </v-share-tools>
+        </span>
+      </div>
+      <p
+        v-if="profile.biography"
+        class="p2"
+        :class="props.staffPage ? '' : 'truncate t3lines'"
+      >
+        {{ profile.biography }}
+      </p>
+      <v-flexible-link v-if="showCta" :to="profileLink" class="type-textlink1">
+        {{ ctaText }}
+      </v-flexible-link>
+    </div> -->
   </div>
 </template>
 
 <style lang="scss">
-@mixin aspect-ratio($width, $height) {
-  position: relative;
-  &:before {
-    display: block;
-    content: '';
-    width: 100%;
-    padding-top: calc(($height / $width) * 100%);
+.author-profile {
+  gap: 1.5rem;
+  .col-fixed {
+    max-width: v-bind(imageSizePx) !important;
+    @include media('<md') {
+      max-width: v-bind(profileImageSizeSm) !important;
+    }
+    .profile {
+      box-sizing: content-box;
+      width: v-bind(imageSizePx);
+      @include media('<md') {
+        width: v-bind(profileImageSizeSm);
+      }
+    }
   }
-  > .iframeHolder {
-    position: absolute !important;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+  .author-image {
+    background: #ffffff;
+    width: v-bind(imageSizePx);
+    height: v-bind(imageSizePx);
+    border-radius: 50%;
+    margin-right: 12px;
+    overflow: hidden;
+    img {
+      width: v-bind(imageSizePx);
+      height: v-bind(imageSizePx);
+    }
+    @include media('<md') {
+      width: #{v-bind(profileImageSizeSm) px};
+      height: v-bind(profileImageSizeSm);
+      img {
+        width: v-bind(profileImageSizeSm);
+        height: v-bind(profileImageSizeSm);
+      }
+    }
   }
-}
-
-.person {
-  transition: all 1.5s;
-  -webkit-transition: all 1.5s;
-  &.animate {
-    -moz-transform: scale(0.85);
-    -webkit-transform: scale(0.85);
-    -o-transform: scale(0.85);
-    -ms-transform: scale(0.85);
-    transform: scale(0.85);
-    opacity: 0;
-  }
-  .person-inner {
-    color: var(--text-color);
-    .person-image-link {
-      padding: 0;
-      position: relative;
-      line-height: 0;
-      div.circle {
-        position: relative;
-        display: block;
-        border-radius: 100%;
-        overflow: hidden;
-      }
-      &.disabled {
-        pointer-events: none;
-      }
-    }
-    .person-image {
-      height: 100%;
-      width: 100%;
-      object-fit: cover;
-    }
-    .visual-holder {
-      position: relative;
-      display: block;
-      .visual {
-        background: transparent;
-        canvas {
-          display: none;
-          &.show {
-            display: block;
-          }
-        }
-      }
-    }
-    .video-play-button {
-      position: absolute;
-      bottom: spacing(1);
-      left: spacing(1);
-      min-width: 40px;
-      min-height: 40px;
-      .p-button-label {
-        display: none;
-      }
-      &.circle {
-        bottom: 0;
-        left: 0px;
-        top: 70%;
-        right: 70%;
-        margin: auto;
-      }
-    }
-    .person-details {
-      color: var(--text-color);
-      .person-name-link-holder {
-        margin-bottom: spacing(1);
-        .person-name-link {
-          // @include font-config($h2-config);
-          color: var(--text-color);
-          overflow-wrap: anywhere;
-          word-break: break-word;
-          text-decoration: none;
-          display: inline-block;
-          span {
-            transition: color var(--transition-duration);
-            -webkit-transition: color var(--transition-duration);
-          }
-          &.disabled {
-            pointer-events: none;
-          }
-          &:hover {
-            span {
-              color: var(--link-button-hover-color);
-              text-decoration: underline;
-            }
-          }
-        }
-      }
-
-      .person-role,
-      .person-role a {
-        color: inherit;
-        text-decoration: none;
-        // @include font-config($footer-config);
-        text-transform: uppercase;
-        overflow-wrap: anywhere;
-        word-break: break-word;
-        letter-spacing: 1.6px;
-        margin-bottom: spacing(3);
-        &.no-link {
-          pointer-events: none;
-        }
-        &.organization.link {
-          &:hover {
-            text-decoration: underline;
-          }
-        }
-      }
-      .blurbHolder {
-        overflow: hidden;
-        .blurb {
-          // @include font-config($p-config);
-          &.truncate {
-            display: -webkit-box;
-            -webkit-line-clamp: var(--trunc-lines);
-            -webkit-box-orient: vertical;
-            overflow: hidden;
-          }
-          margin-top: spacing(1);
-          overflow-wrap: anywhere;
-          word-break: break-word;
-          &.expanded {
-            -webkit-line-clamp: unset;
-          }
-        }
-      }
-
-      .read-more {
-        word-break: keep-all;
-        cursor: pointer;
-        color: var(--text-color);
-        font-size: var(--font-size-4);
-        letter-spacing: 0.6px;
-        text-transform: uppercase;
-        margin-top: spacing(1);
-        &.show-me {
-          display: inline-block;
-        }
-        &:hover {
-          color: var(--primary-color);
-        }
-      }
-    }
-    .video-holder {
-      position: relative;
-      display: block;
-      margin: 15px auto 0 auto;
-      width: 100%;
-      .iframeHolder {
-        width: 100% !important;
-        height: 100% !important;
-        iframe {
-          width: 100% !important;
-          height: 100% !important;
-        }
-      }
-      @include aspect-ratio(16, 9);
-      .closer {
-        width: 24px;
-        height: 24px;
-        padding: 0;
-        font-size: var(--font-size-2);
-        position: absolute;
-        cursor: pointer;
-        top: spacing(1);
-        right: spacing(1);
-        .p-button-label {
-          display: none;
-        }
-      }
+  &.staffPage {
+    flex-direction: row-reverse;
+    .flexible-link {
+      pointer-events: none;
     }
   }
 }
--- v2/stories/VPerson.stories.js
+++ v2/stories/VPerson.stories.js
@@ -16,28 +16,104 @@ export default {
     },
 }
 
-const social = [
-    {
-        service: 'facebook',
-        profileUrl: 'https://www.facebook.com/WNYC',
+const profile = {
+    "id": 19,
+    "meta": {
+        "first_published_at": "2021-12-06T13:34:20.421614-05:00",
+        "type": "people.PersonPage",
+        "detail_url": "https://cms.demo.nypr.digital/api/v2/pages/19/",
+        "html_url": "http://cms.prod.nypr.digital/staff/scott-lynch/",
+        "slug": "scott-lynch",
+        "show_in_menus": true,
+        "seo_title": "",
+        "search_description": "",
+        "parent": {
+            "id": 6,
+            "meta": {
+                "type": "people.PersonIndexPage",
+                "detail_url": "https://cms.demo.nypr.digital/api/v2/pages/6/",
+                "html_url": "http://cms.prod.nypr.digital/staff/"
+            },
+            "title": "Staff"
+        }
     },
-    {
-        service: 'twitter',
-        profileUrl: 'https://twitter.com/WNYC'
+    "title": "Scott Lynch",
+    "listing_title": "",
+    "listing_summary": "",
+    "listing_image": null,
+    "social_image": null,
+    "social_title": "",
+    "social_text": "",
+    "show_on_index_listing": true,
+    "uuid": "163e9257-5f83-4326-b55a-d367ce10d423",
+    "url": "https://gothamist-vue3demo.gothamist.com/staff/scott-lynch",
+    "first_name": "Scott",
+    "last_name": "Lynch",
+    "photo": {
+        "id": 327700,
+        "meta": {
+            "type": "images.CustomImage",
+            "detail_url": "https://cms.demo.nypr.digital/api/v2/images/327700/",
+            "download_url": "https://cdn.cms.demo.nypr.digital/original_images/B9898331-C616-45E9-A940-B7B092A7163D.JPG"
+        },
+        "title": "B9898331-C616-45E9-A940-B7B092A7163D.JPG"
     },
-    {
-        service: 'instagram',
-        profileUrl: 'https://www.instagram.com/WNYC/'
-    },
-    {
-        service: 'linkedin',
-        profileUrl: 'https://www.linkedin.com/company/wnyc-radio/'
-    },
-    {
-        service: 'youtube',
-        profileUrl: 'https://www.youtube.com/channel/UCbysmY4hyViQAAYEzOR-uCQ'
-    }
-]
+    "job_title": "Photojournalist",
+    "social_media_profile": [
+        {
+            "id": 26,
+            "meta": {
+                "type": "people.SocialMediaProfile"
+            },
+            "profile_url": "https://www.instagram.com/scoboco/",
+            "service": "instagram"
+        },
+        {
+            "id": 48,
+            "meta": {
+                "type": "people.SocialMediaProfile"
+            },
+            "profile_url": "https://twitter.com/Scoboco",
+            "service": "twitter"
+        },
+        {
+            "id": 49,
+            "meta": {
+                "type": "people.SocialMediaProfile"
+            },
+            "profile_url": "https://facebook.com/Scoboco",
+            "service": "facebook"
+        },
+        {
+            "id": 50,
+            "meta": {
+                "type": "people.SocialMediaProfile"
+            },
+            "profile_url": "https://linkedin.com/Scoboco",
+            "service": "linkedin"
+        },
+        {
+            "id": 51,
+            "meta": {
+                "type": "people.SocialMediaProfile"
+            },
+            "profile_url": "https://youtube.com/Scoboco",
+            "service": "youtube"
+        }
+    ],
+    "website": "http://t.co/Np4U39BYlh",
+    "email": "scoboco@gmail.com",
+    "phone_numbers": [
+        {
+            "id": 1,
+            "meta": {
+                "type": "people.PersonPagePhoneNumber"
+            },
+            "phone_number": "9731231234"
+        }
+    ],
+    "biography": "Photojournalist, food writer, preschool receptionist. Love NYC."
+}
 
 const Template = (args) => ({
     components: { VPerson },
@@ -49,144 +125,5 @@ const Template = (args) => ({
 
 export const DefaultHorizontal = Template.bind({})
 DefaultHorizontal.args = {
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-}
-
-export const NoImage = Template.bind({})
-NoImage.args = {
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-}
-
-export const VerticalLayout = Template.bind({})
-VerticalLayout.args = {
-    orientation: 'vertical',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-}
-
-export const ResponsiveLayout = Template.bind({})
-ResponsiveLayout.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-}
-
-export const CircleImage = Template.bind({})
-CircleImage.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    circle: true,
-}
-
-export const Organization = Template.bind({})
-Organization.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    organization: 'Gothamist',
-}
-
-export const OrganizationLinked = Template.bind({})
-OrganizationLinked.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    organization: 'Gothamist',
-    organizationLink: 'https://gothamist.com/',
-}
-
-export const Blurb = Template.bind({})
-Blurb.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    organization: 'Gothamist',
-    organizationLink: 'https://gothamist.com/',
-    blurb: 'Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order.',
-}
-
-export const BlurbTruncated = Template.bind({})
-BlurbTruncated.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    organization: 'Gothamist',
-    organizationLink: 'https://gothamist.com/',
-    blurb: 'Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order. Jen grew up in New Jersey and co-founded Gothamist in 2003 as a way to learn more about New York City beyond Law & Order.',
-    truncate: '2'
-}
-
-export const Website = Template.bind({})
-Website.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    organization: 'Gothamist',
-    organizationLink: 'https://gothamist.com/',
-    websiteUrl: 'https://www.google.com',
-    websiteLabel: 'Website Label',
-
-}
-
-export const WebsiteAndEmail = Template.bind({})
-WebsiteAndEmail.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    organization: 'Gothamist',
-    organizationLink: 'https://gothamist.com/',
-    websiteUrl: 'https://www.google.com',
-    websiteLabel: 'Website Label',
-    email: 'jchung@nypr.com',
-
-}
-
-export const WebsiteAndEmailAndPhoneNumbers = Template.bind({})
-WebsiteAndEmailAndPhoneNumbers.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    organization: 'Gothamist',
-    organizationLink: 'https://gothamist.com/',
-    websiteUrl: 'https://www.google.com',
-    websiteLabel: 'Website Label',
-    email: 'jchung@nypr.com',
-    phoneNumbers: [
-        { phoneNumber: '1-973-615-6339' },
-        { phoneNumber: '1-973-123-1234' }
-    ],
-
-}
-
-export const WebsiteAndEmailAndPhoneNumbersAndSocial = Template.bind({})
-WebsiteAndEmailAndPhoneNumbersAndSocial.args = {
-    orientation: 'responsive',
-    image: 'https://cms.prod.nypr.digital/images/328822/fill-%width%x%height%|format-jpeg|jpegquality-%quality%/',
-    fullName: 'Jen Chung',
-    role: 'Executive Editor and Co-founder',
-    organization: 'Gothamist',
-    organizationLink: 'https://gothamist.com/',
-    websiteUrl: 'https://www.google.com',
-    websiteLabel: 'Website Label',
-    email: 'jchung@nypr.com',
-    phoneNumbers: [
-        { phoneNumber: '1-973-615-6339' },
-        { phoneNumber: '1-973-123-1234' }
-    ],
-    social: social,
-
+    profileId: 19,
 }
