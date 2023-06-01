82437ba4816b3232feb9282c2a76c05e922c48c3
Thomas Bono <thomasbono.com@gmail.com>
2023-06-01 16:06:04 -0400

    VByline images update via slot

--- v2/src/components/VByline.vue
+++ v2/src/components/VByline.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import VBylineUnit from './VBylineUnit.vue'
 const props = defineProps({
   authors: {
@@ -21,8 +22,17 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  flexDirection: {
+    type: String,
+    default: 'row',
+  },
 })
 const emit = defineEmits(['name-click', 'organization-click'])
+
+//css vars
+const cssAlignOption = ref(
+  props.flexDirection === 'row' ? 'center' : 'flex-start'
+)
 
 // depending on the author object, we return a unique key
 const getUniqueKey = (author) => {
@@ -34,92 +44,71 @@ const getUniqueKey = (author) => {
 
 <template>
   <div class="v-byline">
-    <template v-if="showImage">
+    <div class="images-holder" v-if="showImage">
       <template
         v-for="(author, index) in props.authors"
         :key="`author-${index}-${getUniqueKey(author)}`"
       >
-        <slot name="images" />
+        <slot name="images" :author="author" />
       </template>
-    </template>
-    <!-- <template v-if="Array.isArray(props.authors)">
-      <template
-        v-for="(author, index) in props.authors"
-        :key="`author-${index}-${getUniqueKey(author)}`"
-      >
-        <div class="author-image">
-          <v-flexible-link
-            :to="author.url"
-            raw
-            :aria-hidden="true"
-            :tabindex="-1"
-            @click="emit('click-image', author.url)"
+    </div>
+    <div>
+      {{ props.prefix }}
+      <template v-if="Array.isArray(props.authors)">
+        <template
+          v-for="(author, index) in props.authors"
+          :key="`author-${index}-${getUniqueKey(author)}`"
+        >
+          <v-byline-unit
+            class="v-byline-unit"
+            :author="author || null"
+            @name-click="($event) => emit('name-click', $event)"
+            @organization-click="($event) => emit('organization-click', $event)"
+          />
+          <span
+            v-if="authors.length > 1 && index < authors.length - 2"
+            class="v-byline-concat"
           >
-            <v-simple-responsive-image
-              v-if="profile.photoID"
-              :src="`${props.imgApi}${profile.photoID}${props.imgApiSuffix}`"
-              :width="imageSize"
-              :height="imageSize"
-              :sizes="props.pixelDensitySizes"
-              :ratio="props.imageRatio"
-              :loading="props.loading"
-              alt="Profile image"
-            />
-            <img
-              v-else
-              :src="`${
-                props.sponsored
-                  ? profile.logo
-                  : props.imageFallbackPath
-                  ? props.imageFallbackPath
-                  : defaultUserPhoto
-              }`"
-              :loading="props.loading"
-              style="width: 100%; height: auto"
-              alt="Profile image"
-            />
-          </v-flexible-link>
-        </div>
+            {{ props.concat }}
+          </span>
+          <span
+            v-else-if="index !== authors.length - 1"
+            class="v-byline-concat"
+          >
+            {{ props.concatLast }}
+          </span>
+        </template>
       </template>
-    </template> -->
-    {{ props.prefix }}
-    <template v-if="Array.isArray(props.authors)">
-      <template
-        v-for="(author, index) in props.authors"
-        :key="`author-${index}-${getUniqueKey(author)}`"
-      >
+      <template v-else>
         <v-byline-unit
           class="v-byline-unit"
-          :author="author || null"
+          :author="props.authors"
           @name-click="($event) => emit('name-click', $event)"
           @organization-click="($event) => emit('organization-click', $event)"
         />
-        <span
-          v-if="authors.length > 1 && index < authors.length - 2"
-          class="v-byline-concat"
-        >
-          {{ props.concat }}
-        </span>
-        <span v-else-if="index !== authors.length - 1" class="v-byline-concat">
-          {{ props.concatLast }}
-        </span>
       </template>
-    </template>
-    <template v-else>
-      <v-byline-unit
-        class="v-byline-unit"
-        :author="props.authors"
-        @name-click="($event) => emit('name-click', $event)"
-        @organization-click="($event) => emit('organization-click', $event)"
-      />
-    </template>
+    </div>
+    <div class="slot-holder">
+      <slot name="afterNames" :author="author" />
+    </div>
+  </div>
+  <div class="v-byline-slot-holder">
+    <slot name="belowNames" :author="author" />
   </div>
 </template>
 
 <style lang="scss">
 .v-byline {
   color: var(--text-color);
-
+  display: flex;
+  gap: 10px;
+  align-items: v-bind(cssAlignOption);
+  flex-direction: v-bind(flexDirection);
+  .images-holder {
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+  }
   .v-byline-contributing-org,
   .v-byline-concat,
   .v-byline-unit {
--- v2/stories/VByline.stories.js
+++ v2/stories/VByline.stories.js
@@ -1,4 +1,5 @@
 import VByline from '../src/components/VByline.vue'
+import VSimpleResponsiveImage from '../src/components/VSimpleResponsiveImage.vue'
 
 export default {
     title: 'Components-V2/VByline',
@@ -24,15 +25,52 @@ const Template = (args) => ({
     template: '<v-byline v-bind="args" />',
 })
 
+
 const TemplateImage = (args) => ({
+    components: { VByline, VSimpleResponsiveImage },
+    setup() {
+        return { args }
+    },
+    template: '<v-byline v-bind="args"> <template #images="slotProps" ><VSimpleResponsiveImage :src="`https://cms.prod.nypr.digital/images/${slotProps.author.photoID}/fill-%width%x%height%|format-webp|webpquality-%quality%`" :width="48" :height="48" :ratio="[1,1]" alt="Profile image" style="width:48px; height:auto;"/></template></v-byline>',
+})
+
+const TemplateBelowNameSlot = (args) => ({
     components: { VByline },
     setup() {
         return { args }
     },
-    template: '<v-byline v-bind="args"> <template #images>hello</template></v-byline>',
+    template: '<v-byline v-bind="args"> <template #belowNames="slotProps" >SLOT BELOW THE NAMES</template></v-byline>',
 })
-const authorsArray = [
 
+const TemplateAfterNameSlot = (args) => ({
+    components: { VByline },
+    setup() {
+        return { args }
+    },
+    template: '<v-byline v-bind="args"> <template #afterNames="slotProps" >SLOT AFTER THE NAMES</template></v-byline>',
+})
+const authorArray = [
+    {
+        "id": 146821,
+        "firstName": "Nsikan",
+        "lastName": "Akpan",
+        "name": "Nsikan Akpan",
+        "photoID": 327577,
+        "jobTitle": "Editor, Health & Science",
+        "biography": "Nsikan runs the health and science desk at WNYC/Gothamist. Nsikan was previously at National Geographic where he worked as a science editor, overseeing its COVID-19 coverage in addition to other topics in science, health and technology. Before National Geographic, he worked for more than four years at PBS NewsHour, where he co-created an award-winning video series named ScienceScope. \r\n\r\nHe shared a 2020 Emmy for the PBS NewsHour series “Stopping a Killer Pandemic” and in 2019 received a George Foster Peabody Award for the PBS NewsHour series “The Plastic Problem.” Nsikan has also worked for NPR, Science News Magazine, Science Magazine, KUSP Central Coast Public Radio, the Santa Cruz Sentinel and as a writer at the Center for Infection and Immunity at Columbia University. He holds a doctorate in pathobiology from Columbia University and is an alum of the science communication program at the University of California, Santa Cruz.",
+        "website": "",
+        "email": "",
+        "slug": "nsikan-akpan",
+        "url": "/staff/nsikan-akpan",
+        "socialMediaProfile": [
+            {
+                "service": "twitter",
+                "profileUrl": "https://twitter.com/MoNscience"
+            }
+        ]
+    }
+]
+const authorsArray = [
     {
         "id": 121,
         "firstName": "Jaclyn",
@@ -66,43 +104,7 @@ const authorsArray = [
             }
         ]
     }
-
 ]
-// const authorsArray = [
-//     {
-//         id: '1',
-//         firstName: 'Jen',
-//         lastName: 'Chung',
-//         name: 'Jen Chung',
-//         organization: 'Gothamist',
-//         organizationUrl: 'http://www.gothamist.com',
-//         url: '/jen-chung'
-//     },
-//     {
-//         id: '2',
-//         firstName: 'Jen',
-//         lastName: 'Chung2',
-//         name: 'Jen Chung 2',
-//         organization: 'Gothamist',
-//         url: '/jen-chung2'
-//     },
-//     {
-//         id: '3',
-//         firstName: 'Brian',
-//         lastName: 'Lehrer',
-//         name: 'Brian Lehrer',
-//         url: '/brian-lehrer',
-//     },
-//     {
-//         id: '4',
-//         firstName: 'Trinity',
-//         lastName: 'Anderson',
-//         name: 'Trinity Anderson',
-//         url: '/trinity-anderson',
-//         organization: 'Gothamist',
-//         organizationUrl: 'http://www.gothamist.com',
-//     }
-// ]
 
 const authorsObject =
 {
@@ -144,6 +146,20 @@ CustomConcatsNoPrefix.args = {
 }
 export const imageSlot = TemplateImage.bind({})
 imageSlot.args = {
-    authors: authorsArray,
+    authors: authorArray,
     showImage: true,
+}
+export const imagesSlot = TemplateImage.bind({})
+imagesSlot.args = {
+    authors: authorsArray,
+    flexDirection: 'column',
+    showImage: true,
+}
+export const BelowNameSlot = TemplateBelowNameSlot.bind({})
+BelowNameSlot.args = {
+    authors: authorsArray,
+}
+export const AfterNameSlot = TemplateAfterNameSlot.bind({})
+AfterNameSlot.args = {
+    authors: authorsArray,
 }
