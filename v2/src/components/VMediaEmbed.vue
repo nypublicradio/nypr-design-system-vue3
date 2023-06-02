df5126776ca529bd01229e2a9ed87d6f63b26560
Thomas Bono <thomasbono.com@gmail.com>
2023-06-02 01:06:14 -0400

    added spotify and soundcloud to media embed

--- v2/src/components/VMediaEmbed.vue
+++ v2/src/components/VMediaEmbed.vue
@@ -1,7 +1,84 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  label: {
+    type: String,
+    default: 'embedded video',
+  },
+  url: {
+    type: String,
+    default: null,
+  },
+  ratio: {
+    type: Array,
+    default: () => [16, 9],
+  },
+  controls: {
+    type: Boolean,
+    default: true,
+  },
+  autoPlay: {
+    type: Boolean,
+    default: false,
+  },
+  startTime: {
+    type: String,
+    default: '0',
+  },
+  mute: {
+    type: Boolean,
+    default: false,
+  },
+})
+const getRatio = computed(() => {
+  var percent = (100 / props.ratio[0]) * props.ratio[1]
+  return percent + '%'
+})
+
+const isSpotify = computed(() => {
+  return props.url.includes('spotify.com')
+})
+const isSoundCloud = computed(() => {
+  return props.url.includes('soundcloud.com')
+})
+const getSoundCloudUrl = computed(() => {
+  return props.autoPlay
+    ? props.url.replace('auto_play=false', 'auto_play=true')
+    : props.url
+})
+</script>
+
 <template>
   <div class="responsive-embed">
     <iframe
-      :src="`${url}?controls=${controls ? '1' : '0'}&amp;start=${startTime}&amp;autoplay=${autoPlay ? '1' : '0'}&amp;mute=${mute ? '1' : '0'}`"
+      v-if="isSpotify"
+      style="border-radius: 12px"
+      :src="props.url"
+      title="Spotify Player"
+      width="100%"
+      height="352"
+      frameBorder="0"
+      allowfullscreen=""
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+    <iframe
+      v-else-if="isSoundCloud"
+      width="100%"
+      height="200"
+      scrolling="no"
+      frameborder="no"
+      allow="autoplay"
+      :src="getSoundCloudUrl"
+    ></iframe>
+
+    <iframe
+      v-else
+      :src="`${url}?controls=${
+        controls ? '1' : '0'
+      }&amp;start=${startTime}&amp;autoplay=${autoPlay ? '1' : '0'}&amp;mute=${
+        mute ? '1' : '0'
+      }`"
       title="Media Player"
       :aria-label="label"
       frameborder="0"
@@ -10,45 +87,6 @@
     ></iframe>
   </div>
 </template>
-
-<script setup>
-import { computed } from 'vue'
-const props = defineProps({
-  label: {
-    type: String,
-    default: 'embedded video'
-  },
-  url: {
-    type: String,
-    default: null
-  },
-  ratio: {
-    type: Array,
-    default: () => [16, 9]
-  },
-  controls: {
-    type: Boolean,
-    default: true
-  },
-  autoPlay: {
-    type: Boolean,
-    default: false
-  },
-  startTime: {
-    type: String,
-    default: "0"
-  },
-  mute: {
-    type: Boolean,
-    default: false
-  }
-})
-const getRatio = computed(() => {
-  var percent = 100 / props.ratio[0] * props.ratio[1]
-  return percent + "%"
-})
-
-</script>
 
 <style lang="scss">
 .responsive-embed {
--- v2/stories/VMediaEmbed.stories.js
+++ v2/stories/VMediaEmbed.stories.js
@@ -71,3 +71,14 @@ YoutubeAutoplayMuted.args = {
     mute: true,
 }
 
+export const Spotify = Template.bind({})
+Spotify.args = {
+    url: 'https://open.spotify.com/embed/track/5F7I0CfdEnVSPlwFabMyIi?utm_source=generator',
+}
+export const Soundcloud = Template.bind({})
+Soundcloud.args = {
+    url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13165175&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+    autoPlay: false,
+    ratio: [3, 1]
+}
+
