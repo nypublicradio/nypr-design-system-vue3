91436e42768b284206e877bc7965a5cb6bbc59f8
bonomite <thomasbono.com@gmail.com>
2024-01-05 11:23:52 -0500

    added crossorigin prop

--- v2/src/components/VNewPersistentPlayer.vue
+++ v2/src/components/VNewPersistentPlayer.vue
@@ -632,7 +632,7 @@ defineExpose({
             :volume="props.volume"
             :loop="props.loop"
             poster="https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_3x2.jpg?w=1638&h=1092"
-            crossorigin
+            crossorigin="use-credentials"
             keep-alive
             :prefer-native-h-l-s="true"
           >
