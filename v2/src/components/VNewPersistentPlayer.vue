877f351aea39e994e30cecdc793c6b740d76d951
bonomite <thomasbono.com@gmail.com>
2024-01-05 12:00:32 -0500

    player props updated

--- v2/src/components/VNewPersistentPlayer.vue
+++ v2/src/components/VNewPersistentPlayer.vue
@@ -632,9 +632,9 @@ defineExpose({
             :volume="props.volume"
             :loop="props.loop"
             poster="https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_3x2.jpg?w=1638&h=1092"
-            crossorigin="use-credentials"
             keep-alive
             :prefer-native-h-l-s="true"
+            controls
           >
             <media-provider></media-provider>
 
