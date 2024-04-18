54b2a6a9f5810ce02e5d9d7fa968ff651804934f
bonomite <thomasbono.com@gmail.com>
2024-04-18 12:32:36 -0400

    image src determiner

--- v2/src/components/helpers.js
+++ v2/src/components/helpers.js
@@ -6,12 +6,13 @@ export const cmsSources = {
 
 // determines the CMS source of an image
 export const getCmsSource = (src) => {
-  if (src.includes('media.npr.org')) {
-    return cmsSources.NPR
-  } else if (src.includes('media.wnyc.org')) {
-    return cmsSources.PUBLISHER
-  } else if (src.includes('nypr.digital')) {
+  // if src is all just numbers, it's a wagtail image. using the domain for the others
+  if (/^\d+$/.test(src)) {
     return cmsSources.WAGTAIL
+  } else if (src.includes("media.wnyc.org")) {
+    return cmsSources.PUBLISHER
+  } else if (src.includes("media.npr.org")) {
+    return cmsSources.NPR
   } else {
     return cmsSources.WAGTAIL
   }
